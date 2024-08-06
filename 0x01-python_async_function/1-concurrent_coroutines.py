8f40e60360d9300a406fc58f371178416b2770d8
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-07 00:02:32 +0100

    task 1 done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@
 coroutines at the same time with async
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     the basic of async
     """
