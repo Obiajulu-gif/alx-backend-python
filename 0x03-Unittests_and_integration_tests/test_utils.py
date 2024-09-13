b27435e78f936fa92332a6f18a016ae2dc31224a
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-13 15:45:08 +0100

    task 1 done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map with different inputs."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map with invalid inputs."""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), "'{}'".format(path[-1]))
+
 
 if __name__ == '__main__':
     unittest.main()
