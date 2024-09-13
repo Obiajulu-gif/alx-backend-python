5b1f1b04f9ec2b040dc0f486d425e57c5da29d5c
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-13 15:38:56 +0100

    task 0 done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,7 +12,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map with different inputs."""
