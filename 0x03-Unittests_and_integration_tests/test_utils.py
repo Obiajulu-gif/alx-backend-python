1adec34644782b22f470c2bcede4964888b2a372
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-13 16:06:30 +0100

    task 2 done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """TestAccessNestedMap class that inherits from unittest.TestCase"""
 import unittest
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +27,21 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), "'{}'".format(path[-1]))
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that get_json return expected JSON data."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
